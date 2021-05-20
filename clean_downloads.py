c9066e9fc8b2bbff1f56d51d6b7b15087226b369
Evan Wescott <ewescott.21016@gmail.com>
2021-05-20 13:27:35 -0500

    add command line args to direct path

--- clean_downloads.py
+++ clean_downloads.py
@@ -1,16 +1,32 @@
 import time
 import os
+import getopt, sys
 
-os.chdir(r'C:\Users\evan.wescott\Downloads')
-files = os.listdir(os.getcwd())
-present_time = time.time()
-days = 30*24*60*60
+def main(argv):
+    message = 'clean_downloads.py -h -i <filepath>'
+    try:
+        opts, args = getopt.getopt(argv, "hi:")
+    except:
+        print(message)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(message)
+        elif opt in ("-i"):
+            cleanupAction(argv)
 
-for file_name in files:
-    if not os.path.isdir(file_name):
-        access_time = os.stat(file_name).st_atime
-        if access_time < (present_time-days):
-            os.remove(file_name)
-            print(file_name + " removed")
+def cleanupAction(path):
+    os.chdir(str(path))
+    files = os.listdir(os.getcwd())
+    present_time = time.time()
+    days = 30*24*60*60
 
-print("Cleanup complete!")
+    for file_name in files:
+        if not os.path.isdir(file_name):
+            access_time = os.stat(file_name).st_atime
+            if access_time < (present_time-days):
+                os.remove(file_name)
+                print(file_name + " removed")
+        print("Cleanup complete!")
+
+main(sys.argv[1:])
